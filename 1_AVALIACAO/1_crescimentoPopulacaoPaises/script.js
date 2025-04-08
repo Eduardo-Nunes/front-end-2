2406aeff0d9e2f76a4b437cadd34acaaf7b1210c
Eduardo-Nunes <eduardonunes056@gmail.com>
2025-04-07 21:23:27 -0300

    crescimentoPopulacaoPaises

--- 1_AVALIACAO/1_crescimentoPopulacaoPaises/script.js
+++ 1_AVALIACAO/1_crescimentoPopulacaoPaises/script.js
@@ -1,5 +1,21 @@
-function crescimentoPopulacaoPaises(params) {
-    
+function crescimentoPopulacaoPaises() {
+    let populacaoA = 80000;
+    const taxaA = 3;
+
+    let populacaoB = 200000;
+    const taxaB = 1.5;
+
+    let anos = 0;
+
+    while (populacaoA < populacaoB) {
+        populacaoA *= (1 + taxaA / 100);
+        populacaoB *= (1 + taxaB / 100);
+        anos++;
+    }
+
+    console.log({ anos });
+
+    return anos;
 }
 
 crescimentoPopulacaoPaises();
